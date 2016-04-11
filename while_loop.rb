5c3391fd59089ae0898e52fda3e808ee2f27d589
zac <zacmitton22@gmail.com>
2016-04-11 10:03:01 -0500

    added while_loop.rb

---
+++ while_loop.rb
@@ -0,0 +1,17 @@
+# testing for git
+puts "How high to count?"
+n = gets.chomp.to_i
+puts "What number to increment by?"
+i = gets.chomp.to_i
+
+counter = 0
+
+while counter <= n
+  if counter % i != 0
+    puts "#{counter}"
+    counter += 1
+  else
+    puts "got to #{counter}!"
+    counter += 1
+  end
+end
